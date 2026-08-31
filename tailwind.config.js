ba336d853c760080f19f5bdff997ddce830c7848
MSJ <LAWANGENTER@GMAIL.COM>
2026-08-31 19:46:52 +0500

    Update and rename script.js to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/* tailwind.config.js */
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        shooz: {
+          slate: '#2F4454',
+          plum: '#2E151B',
+          rose: '#DA7B93',
+          teal: '#376E6F',
+          darkteal: '#1C3334'
+        }
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif']
+      }
+    }
+  }
+}
